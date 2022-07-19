70bfc8484b009c659046278c64cadf0cd5f5f890
Suraj Sokasane <surajsokasane@Surajs-MacBook-Pro.local>
2022-07-19 22:15:22 +0530

    testCommit - single Class

---
+++ api-investor/InvesterSystem.DataModel/Table/BankDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace InvestorSystem.DataModel.Table
+{
+    public class BankDetails
+    {
+        public int ID { get; set; }
+        public string bankName { get; set; }
+        public string accountNumber { get; set; }
+        public string accountName { get; set; }
+        public string IFSC { get; set; }
+        public int accountTypeID { get; set; }
+    }
+}
+
