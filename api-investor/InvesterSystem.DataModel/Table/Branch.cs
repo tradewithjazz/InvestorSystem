4b51b5ff861a6c7bb8fb4a454bed4b763f966dda
Suraj Sokasane <surajsokasane@Surajs-MacBook-Pro.local>
2022-07-19 22:12:55 +0530

    TestCommit - One Class

---
+++ api-investor/InvesterSystem.DataModel/Table/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InvestorSystem.DataModel.Table
+{
+    public class Branch
+    {
+        [Key]
+        public int ID { get; set; }
+        public string name { get; set; }
+        public int bmID { get; set; }
+        public string address { get; set; }
+    }
+}
+
