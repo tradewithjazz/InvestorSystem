58e86a83fc53140525159136f246ee88b8ab0eb2
Suraj Sokasane <surajsokasane@Surajs-MacBook-Pro.local>
2022-08-03 15:52:13 +0530

    Organized DTOs

---
+++ api-investor/InvestorSystem.Core/Areas/Common/DTOs/BankDetailsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace InvestorSystem.Core.Areas.Common.DTOs
+{
+    public class BankDetailsDTO
+    {
+        public long ID { get; set; }
+        public long InvestorID { get; set; }
+        public string BankName { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string IFSC { get; set; }
+        public short AccountTypeID { get; set; }
+    }
+}
+
---
+++ api-investor/InvestorSystem.Core/Areas/Common/DTOs/NomineeDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace InvestorSystem.Core.Areas.Common.DTOs
+{
+    public class NomineeDTO
+    {
+        public long ID { get; set; }
+        public long InvestorID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public short RelationshipID { get; set; }
+    }
+}
+
